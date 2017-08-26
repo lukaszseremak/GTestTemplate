c5c697df2c1f359a642f4884538f90a54e9ea6ff
Łukasz Seremak <lukasz.seremak@outlook.com>
2017-08-26 22:13:20 +0200

    remove unnecessary file and add .gitmodules

--- cmake-build-debug/lib/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/CLionProjects/GTestTemplate/lib/googlemock
-# Build directory: /home/user/CLionProjects/GTestTemplate/cmake-build-debug/lib/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- cmake-build-debug/lib/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/CLionProjects/GTestTemplate/lib/googletest
-# Build directory: /home/user/CLionProjects/GTestTemplate/cmake-build-debug/lib/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
